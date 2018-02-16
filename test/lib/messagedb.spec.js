4497f36515afae675ed8dd783d2b4ab3796c99f3
tigercosmos <phy.tiger@gmail.com>
2018-02-16 19:12:43 +0800

    date2obj

--- test/lib/messagedb.spec.js
+++ test/lib/messagedb.spec.js
@@ -2,13 +2,13 @@
 
 const assert = require('assert');
 const messagedb = require('../../lib/messagedb');
-const date2json = require('date2json');
+const date2obj = require('date2obj');
 
 describe('=== Check messagedb ===', () => {
 
     it('Test messagedb', done => {
         (async () => {
-            const time = date2json();
+            const time = date2obj();
             const msg = `${time.year}-${time.month}-${time.day}-${time.hour}-${time.minute}-${time.second}`;
             const res = await messagedb.write(msg, 'tests/message')
             assert.equal(res, msg);
