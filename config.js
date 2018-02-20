d8893e95a286471bc3609f5a8fa043182cfdd2de
tigercosmos <phy.tiger@gmail.com>
2018-02-21 00:36:16 +0800

    google map key

--- config.js
+++ config.js
@@ -4,10 +4,12 @@ module.exports = {
     channelSecret: process.env.channelSecret,
     channelAccessToken: process.env.channelAccessToken,
     // open weather map
-    owmApiKey:process.env.owmApiKey,
+    owmApiKey: process.env.owmApiKey,
     // imgur
     imgurClientId: process.env.imgurClientId,
     // firebase database
     firebase: "weather-bot-tw",
-    firebaseSecret: process.env.firebaseSecret
+    firebaseSecret: process.env.firebaseSecret,
+    // Google Map
+    googleMapKey: process.env.googleMapKey
 }
