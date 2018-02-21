096ae2c5e8dc18a86566c569e2eb708c068e5e29
tigercosmos <phy.tiger@gmail.com>
2018-02-21 11:27:22 +0800

    messenger config

--- config.js
+++ config.js
@@ -3,6 +3,9 @@ module.exports = {
     channelId: process.env.channelId,
     channelSecret: process.env.channelSecret,
     channelAccessToken: process.env.channelAccessToken,
+    // messager
+    messagerAccessToken: process.env.messagerAccessToken,
+    messagerAppSecret: process.env.messagerAppSecret,
     // open weather map
     owmApiKey: process.env.owmApiKey,
     // imgur
