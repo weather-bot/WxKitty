0cd25b50954e584140fedccbb2525307ab951b4e
tigercosmos <phy.tiger@gmail.com>
2018-02-25 23:24:33 +0800

    isTaiwanArea test

--- test/lib/keywords.spec.js
+++ test/lib/keywords.spec.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const assert = require('assert');
+const logger = require('node-color-log');
 const {
     isObservation,
     isAirStation,
     isWeather,
-    isFunny
+    isFunny,
+    isTaiwanArea
 } = require('../../lib/keywords');
 
 describe('=== Check keywords ===', () => {
@@ -31,8 +33,43 @@ describe('=== Check keywords ===', () => {
     });
 
     it('Test isFunny', done => {
-        assert.notEqual(isFunny("新年"));
+        assert.notEqual(isFunny("新年"), null);
         assert.equal(isObservation("美國"), null);
+        done();
+    });
+
+    it('Test isTaiwanArea', done => {
+        let msg = "台北士林的天氣狀況";
+        assert.equal(
+            isTaiwanArea(msg).includes("臺北市士林區"),
+            true,
+            logger.debug(`${msg} & ${isTaiwanArea(msg)}`)
+        );
+        msg = "我想知道桃園復興霞雲天氣";
+        assert.equal(
+            isTaiwanArea(msg).includes("桃園市復興區霞雲里"),
+            true,
+            logger.debug(`${msg} & ${isTaiwanArea(msg)}`)
+        );
+        msg = "我想知道中壢天氣";
+        assert.equal(
+            isTaiwanArea(msg).includes("桃園市中壢區"),
+            true,
+            logger.debug(`${msg} & ${isTaiwanArea(msg)}`)
+        );
+        msg = "現在臺南濕度";
+        assert.equal(
+            isTaiwanArea(msg).includes("臺南市"),
+            true,
+            logger.debug(`${msg} & ${isTaiwanArea(msg)}`)
+        );
+        msg = "內湖的天氣";
+        assert.equal(
+            isTaiwanArea(msg).includes("臺北市內湖區"),
+            true,
+            logger.debug(`${msg} & ${isTaiwanArea(msg)}`)
+        );
+        assert.equal(isTaiwanArea("美國紐約"), null);
         done();
     });
 });
