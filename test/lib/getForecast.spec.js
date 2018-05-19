8a2ed4a14f32a8a614a816486c37676c9ebb66d2
tigercosmos <phy.tiger@gmail.com>
2018-05-19 20:50:53 +0800

    36 hours forecast done

--- test/lib/getForecast.spec.js
+++ test/lib/getForecast.spec.js
@@ -6,15 +6,25 @@ const assert = require('assert');
 const  getForecast = require('../../lib/getForecast');
 
 describe('=== Check getForecast ===', () => {
-    it('Case 1', done => {
+    it('Case 1: valid input', done => {
         (async () => {
-            const msg = await getForecast("士林區明天的天氣");
+            let msg = await getForecast("士林區明天的天氣");
             logger.debug(msg);
             assert.equal(msg.includes("臺北市"), true);
             assert.equal(msg.includes("降雨機率"), true);
+
+            msg = await getForecast("台中明天的天氣");
+            logger.debug(msg);
+            assert.equal(msg.includes("臺中市"), true);
+            assert.equal(msg.includes("降雨機率"), true);
+
+            msg = await getForecast("宜蘭明天晚上預報");
+            logger.debug(msg);
+            assert.equal(msg.includes("宜蘭"), true);
+            assert.equal(msg.includes("降雨機率"), true);
             done();
         })();
-    }).timeout(4000);
+    }).timeout(12000);
 
     it('Case 2: No Area', done => {
         (async () => {
@@ -22,7 +32,7 @@ describe('=== Check getForecast ===', () => {
             assert.equal(msg.includes("查不到"), true);
             done();
         })();
-    }).timeout(4000);
+    }).timeout(5000);
 
     it('Case 3: Invalid Time', done => {
         (async () => {
@@ -30,5 +40,5 @@ describe('=== Check getForecast ===', () => {
             assert.equal(msg.includes("查不到"), true);
             done();
         })();
-    }).timeout(4000);
+    }).timeout(1000);
 });
