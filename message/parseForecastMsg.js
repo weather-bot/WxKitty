06b12991c37572eca14e94fe0e60af7237a54a9e
Claire Chang <cochia0318@hotmail.com>
2018-05-29 23:55:19 +0800

    skip data parameter value

--- message/parseForecastMsg.js
+++ message/parseForecastMsg.js
@@ -1,17 +1,23 @@
-function parseForecastMsg(e) {
-    let res = `地區：${e.area}
+module.exports = {
+    parse36HoursMsg : (e)=>{
+        return  `地區：${e.area}
 時間：${e.time}
 描述：${e.weatherDescription} ${e.feelingDesciption}
 最高溫度：${e.maxTemp}℃
 最低溫度：${e.minTemp}℃
-`;
-    if(e.rainProbability != undefined ) {
-       res += `降雨機率：${e.rainProbability}%\n`
-    }
-    res += `---
+降雨機率：${e.rainProbability}%\n
+---
 資料來源：中央氣象局
-`;
-    return res;
-} 
-
-module.exports = parseForecastMsg;
+        `;
+    },
+    parse7DaysMsg : (e)=>{
+        return  `地區：${e.area}
+時間：${e.time}
+描述：${e.weatherDescription}
+最高溫度：${e.maxTemp}℃
+最低溫度：${e.minTemp}℃
+---
+資料來源：中央氣象局
+        `;
+    }
+}
