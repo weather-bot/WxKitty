9bc26da4b8d42edefb45f12f8fc6c0898b4ee5a8
csinrn <csinrn24@gmail.com>
2018-05-30 15:51:23 +0800

    f

--- message/parseForeignForecastMsg.js
+++ message/parseForeignForecastMsg.js
@@ -1,7 +1,7 @@
 function parseForeignForecastMsg(e) {
     return `地區：${e.area}
-    開始時間：${e.Stime}
-    結束時間：${e.Etime}
+開始時間：${e.Stime}
+結束時間：${e.Etime}
 描述：${e.weatherDescription}
 最高溫度：${e.maxTemp}℃
 最低溫度：${e.minTemp}℃
