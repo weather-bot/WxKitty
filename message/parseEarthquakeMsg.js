02760a09815ae15752b7d710147e0a3467eb69e0
csinrn <csinrn24@gmail.com>
2020-05-19 00:11:35 +0800

    FIX: fix getEarthquake()

---
+++ message/parseEarthquakeMsg.js
@@ -0,0 +1,20 @@
+function parseEarthquakeMsg(majorList, mildList){
+    var res = ""
+    if (majorList.length != 0){
+        res += "近日較大地震:\n"
+        majorList.forEach(e =>{
+            res += "⦿ " + e + '\n'
+        })
+    }
+    if (mildList.length != 0){
+        res += "近日一般地震:\n"
+        mildList.forEach(e =>{
+            res += "⦿ " + e + '\n'
+        })
+    }
+
+    res += "---\n資料來源:中央氣象局"
+    return res
+}
+
+module.exports.parseEarthquakeMsg = parseEarthquakeMsg;
