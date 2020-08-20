0fee68b2329b3ed0ee81404786e2df5e2af9cf35
Jennesy <jennesy.dai@gmail.com>
2020-08-21 01:33:19 +0800

    observation fixed

--- message/parseObsStMsg.js
+++ message/parseObsStMsg.js
@@ -1,15 +1,15 @@
 function parseObsStMsg(data) {
     const elementsWanted = {
-        'obsTime': '時間 {}',
-        'TEMP': '溫度 {}℃', 
-        'HUMD': '濕度 {}%',
-        'PRES': '壓力 {}hPa',
-        'WDSD': '風速 {}m/s',
-        'WDIR': '風向 {}',
-        'H_24R': '雨量 {}mm'
+        'obsTime': '時間：{}',
+        'TEMP': '溫度：{}℃', 
+        'HUMD': '濕度：{}%',
+        'PRES': '壓力：{}hPa',
+        'WDSD': '風速：{}m/s',
+        'WDIR': '風向：{}',
+        'H_24R': '雨量：{}mm'
     }
     var reg = /{\w*}/
-    var res = `測站： ${data.locationName}\n`
+    var res = `測站：${data.locationName}\n`
     data.weatherElement.forEach(e => {
         if (elementsWanted[e.elementName] != null) {
             res += elementsWanted[e.elementName].replace(reg, e.elementValue) + '\n'
