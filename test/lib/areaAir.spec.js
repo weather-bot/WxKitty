dd67fcbe38c03a1518d41ec973dcb2fcef6b2044
Jennesy <jennesy.dai@gmail.com>
2020-08-30 01:59:42 +0800

    new Air URL

--- test/lib/areaAir.spec.js
+++ test/lib/areaAir.spec.js
@@ -6,17 +6,34 @@ const assert = require('assert');
 const {
     getAreaAir,
 } = require('../../lib/areaAir');
+const {
+    isForeignAirStation
+} = require('../../lib/keywords')
 
 describe('=== Check getAreaAir ===', () => {
-    it('use CWB station', done => {
+    it('Case 1: taiwan area use CWB station', done => {
         (async () => {
+            const foreignStation = await isForeignAirStation("天母里");
             const msg = await getAreaAir({
                 lon: "121.5361352",
                 lat: "25.13101919",
                 realAreaName: "天母里"
-            });
+            }, foreignStation);
             logger.debug(msg);
             assert.equal(msg.includes("資料來源：環保署"), true);
+            done();
+        })();
+    }).timeout(5000);
+    it('Case 2: taiwan area use foreign station', done => {
+        (async () => {
+            const foreignStation = await isForeignAirStation("深坑區");
+            const msg = await getAreaAir({
+                lon: "121.6091939",
+                lat: "24.98573162",
+                realAreaName: "深坑區"
+            }, foreignStation);
+            logger.debug(msg);
+            assert.equal(msg.includes("資料來源：breezometer.com"), true);
             done();
         })();
     }).timeout(5000);
