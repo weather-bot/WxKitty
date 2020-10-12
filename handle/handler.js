808578366db8eafc86e7de10dfdcf3fb4aa7697a
Jennesy <jennesy.dai@gmail.com>
2020-10-12 14:52:30 +0800

    cloud classify

--- handle/handler.js
+++ handle/handler.js
@@ -21,27 +21,27 @@ const handler = async context => {
     } else if (context.event.isImage || context.event.isPhoto) {
 
         // await platformReplyText(context, "雲辨識功能暫停");
-        // if (context.state.isGotReqWaitImg) {
-        // await cloudClassifyingHandler(context);
-        // // only ask if need to classify the photo when user
-        // // upload a photo in personal mode, otherwise we
-        // // ignore the photo.
-        // } else if (context.platform == "telegram" || !(context.event.rawEvent.source.type == 'room' ||
-        // context.event.rawEvent.source.type == 'group')) {
-        // context.setState({
-        // isGotImgWaitAns: true,
-        // isGotReqWaitImg: false,
-        // previousContext: {
-        // platform: context.platform,
-        // id: getImageId(context),
-        // token: getPlatformToken(context.platform)
-        // }
-        // })
-        // await platformReplyText(
-        // context,
-        // require('../message/isImageMsg')
-        // );
-        // }
+        if (context.state.isGotReqWaitImg) {
+            await cloudClassifyingHandler(context);
+            //  only ask if need to classify the photo when user
+            //  upload a photo in personal mode, otherwise we
+            //  ignore the photo.
+        } else if (context.platform == "telegram" || !(context.event.rawEvent.source.type == 'room' ||
+                context.event.rawEvent.source.type == 'group')) {
+            context.setState({
+                isGotImgWaitAns: true,
+                isGotReqWaitImg: false,
+                previousContext: {
+                    platform: context.platform,
+                    id: getImageId(context),
+                    token: getPlatformToken(context.platform)
+                }
+            })
+            await platformReplyText(
+                context,
+                require('../message/isImageMsg')
+            );
+        }
     } else if (context.event.isText) {
         await textHandler(context, context.event.text);
     }
